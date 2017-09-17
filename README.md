3dced2ac006adcd0f1cde0ba9b39a4f29f31f6f4
George Talusan <george.talusan@gmail.com>
2017-09-16 23:43:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# nRF5x DS18B20 example
+
+![alt text](./redbear-ds18b20.jpg "DS18B20 with Redbear BLE Nano v2")
+
+### Description
+
+Short example broadcasting a DS18B20 temperature sensor over Bluetooth LE
+
+### Electrical
+
+DS18B20 wired in parasitic mode.  3.3V -> 2.2kΩ resistor -> DQ, DQ -> D4 (nRF).  VCC shorted to GND.
---
+++ nrf52-ds18b20.ino
@@ -0,0 +1,42 @@
+#include <BLEPeripheral.h>
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 4
+
+BLEPeripheral ble;
+
+BLEService tempService("CCC0");
+BLEFloatCharacteristic tempCharacteristic("CCC1", BLERead | BLENotify);
+BLEDescriptor tempDescriptor("2901", "Temperature");
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+static unsigned long then = millis();
+
+void setup(void)
+{
+	Serial.begin(9600);
+
+	ble.setDeviceName("BLE/DS18B20");
+	ble.setAdvertisedServiceUuid(tempService.uuid());
+	ble.addAttribute(tempService);
+	ble.addAttribute(tempCharacteristic);
+	ble.addAttribute(tempDescriptor);
+
+	sensors.begin();
+	ble.begin();
+}
+
+void loop(void)
+{
+	ble.poll();
+	unsigned long now = millis();
+	if (then - now >= 30 * 1000) {
+		sensors.requestTemperatures();
+		tempCharacteristic.setValue(sensors.getTempCByIndex(0));
+		then = now;
+	}
+}
