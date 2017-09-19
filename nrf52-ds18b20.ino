7a0fb73b704325d6fa4664ebe3112c04415b9cb9
George Talusan <george.talusan@gmail.com>
2017-09-18 22:20:29 -0400

    reset on disconnect so the system can go into low power

--- nrf52-ds18b20.ino
+++ nrf52-ds18b20.ino
@@ -2,18 +2,21 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#include <nrf_nvic.h>
+
 #define ONE_WIRE_BUS 4
 
 BLEPeripheral ble;
 
 BLEService tempService("CCC0");
-BLEFloatCharacteristic tempCharacteristic("CCC1", BLERead | BLENotify);
+BLEFloatCharacteristic tempCharacteristic("CCC1", BLERead/* | BLENotify*/);
 BLEDescriptor tempDescriptor("2901", "Temperature");
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
 static unsigned long then = millis();
+static bool _connected = false;
 
 void setup(void)
 {
@@ -22,6 +25,13 @@ void setup(void)
 	ble.addAttribute(tempService);
 	ble.addAttribute(tempCharacteristic);
 	ble.addAttribute(tempDescriptor);
+	ble.setEventHandler(BLEConnected, [=] (BLECentral &central) {
+		_connected = true;
+	});
+	ble.setEventHandler(BLEDisconnected, [=] (BLECentral &central) {
+		_connected = false;
+		sd_nvic_SystemReset();
+	});
 
 	sensors.begin();
 	ble.begin();
@@ -31,9 +41,16 @@ void loop(void)
 {
 	ble.poll();
 	unsigned long now = millis();
-	if (now - then >= 3 * 1000) {
-		sensors.requestTemperatures();
-		tempCharacteristic.setValue(sensors.getTempCByIndex(0));
-		then = now;
+	if (_connected) {
+		if (now - then >= 3 * 1000) {
+			sensors.requestTemperatures();
+			tempCharacteristic.setValue(sensors.getTempCByIndex(0));
+			then = now;
+		}
+	}
+	else {		
+		sd_power_dcdc_mode_set(NRF_POWER_DCDC_ENABLE);
+		sd_power_mode_set(NRF_POWER_MODE_LOWPWR);
+		sd_app_evt_wait();
 	}
 }
